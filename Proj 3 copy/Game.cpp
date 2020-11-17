5afab2674defec5aa40f40b7f40f27bae884ba1a
adamtheromack <74581155+adamtheromack@users.noreply.github.com>
2020-11-16 21:49:22 -0700

    did stuff

--- Proj 3 copy/Game.cpp
+++ Proj 3 copy/Game.cpp
@@ -1,5 +1,62 @@
 #include <iostream>
+#include "Game.h"
 using namespace std;
 
-// hehe penis
 //This is a new comment
+
+ Game::Game(string playa, string oppo, string playerparty, string oppoparty){
+        srand(time(NULL));
+        turn = 0;
+
+
+
+        Player p(playa, playerparty);
+        Opponent o(oppo, oppoparty);
+
+        
+        // sets up player and opponent objects for the game.
+
+        // Set up the region objects with their random ideas
+
+        // 0 is authority
+        // 1 is personality
+        // 2 is economy
+        // 3 is support
+
+        // for the south,
+        int a = rand() % 2;
+
+        if( a == 0){
+            South s1(0);
+            SouthEast se1(1);
+        }
+        else
+        {
+            South s1(1);
+            SouthEast se1(0);
+        }
+
+        
+
+        int b = rand() % 2;
+        if( b == 0){
+            North n1(2);
+            West w1(3);
+
+        }
+        else{
+            North n1(3);
+            West w1(2);
+
+        }
+
+
+
+
+
+    }
+
+    Game::playerTurn(){
+
+    }
+    Game::
--- Proj 3 copy/Game.h
+++ Proj 3 copy/Game.h
@@ -1,9 +1,26 @@
 #ifndef GAME_H_
 #define GAME_H_
 #include <string>
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+#include "Player.h"
+#include "Opponent.h"
+#include "North.h"
+#include "South.h"
+#include "SouthEast.h"
+#include "West.h"
+
 using namespace std;
 
 class Game{
+
+    private:
+
+
+    public:
+
+    Game(string playa, string oppo, string playerparty, string oppoparty);
 
 };
 
